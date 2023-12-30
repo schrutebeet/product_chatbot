4978e5ffd85745c51eea4441bf47293c745c5987
schrutebeet <ricardosans98@gmail.com>
2023-12-30 03:08:52 +0100

    Add for loop for extracting all categories

--- src/main.py
+++ src/main.py
@@ -7,21 +7,22 @@ from utils.default_columns import default_cols
 
 
 def main():
-    data_extractor = ECIGenericExtractor("https://www.elcorteingles.es/api/firefly/vuestore/products_list/")
-    moda_mujer = data_extractor.iterate_thru_pages('moda-mujer')
-
-    # eci_supermarket = ECISupermarketExtractor("https://www.elcorteingles.es/alimentacion/api/catalog/get-page/supermercado")
-    # eci_supermarket = eci_supermarket.iterate_thru_pages()
-
-    # mercadona_supermarket = MercadonaExtractor("https://tienda.mercadona.es/api/categories/")
-    # mercadona_supermarket = mercadona_supermarket.iterate_thru_categories()
-
     db_utils = UtilsDB()
     db_utils.create_new_models()
-    # db_utils.insert_dict_in_db(eci_supermarket, ECISupermarket)
-    # db_utils.insert_dict_in_db(mercadona_supermarket, Mercadona)
-    ModaMujer = db_utils.create_specific_model('ModaMujer', 'moda_mujer', 'elCorteIngles', default_cols)
-    db_utils.insert_dict_in_db(moda_mujer, ModaMujer)
+    data_extractor = ECIGenericExtractor("https://www.elcorteingles.es/api/firefly/vuestore/products_list/")
+    for category in data_extractor.categories:
+        category_dict = data_extractor.iterate_thru_pages(category)
+        category_model = db_utils.create_specific_model(category, category, 'elCorteIngles', default_cols)
+        db_utils.insert_dict_in_db(category_dict, category_model)
+
+
+    eci_supermarket = ECISupermarketExtractor("https://www.elcorteingles.es/alimentacion/api/catalog/get-page/supermercado")
+    eci_supermarket = eci_supermarket.iterate_thru_pages()
+    db_utils.insert_dict_in_db(eci_supermarket, ECISupermarket)
+
+    mercadona_supermarket = MercadonaExtractor("https://tienda.mercadona.es/api/categories/")
+    mercadona_supermarket = mercadona_supermarket.iterate_thru_categories()
+    db_utils.insert_dict_in_db(mercadona_supermarket, Mercadona)
 
 # CONSUM
 # https://tienda.consum.es/api/rest/V1.0/shopping/category/menu
