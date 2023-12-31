630b6d50f8209f30f0aff19663b3c8d40cf16cb4
Ricardo Sans <ricardosans98@gmail.com>
2023-12-31 20:17:53 +0100

    Reestructure main

--- src/main.py
+++ src/main.py
@@ -6,26 +6,29 @@ from src.data_extractors.corte_ingles.eci_generic_extractor import ECIGenericExt
 from src.data_extractors.corte_ingles.eci_supermarket_extractor import ECISupermarketExtractor
 from src.data_extractors.mercadona.mercadona_extractor import MercadonaExtractor
 from utils.default_columns import default_cols
+from email_notifications.email_generator import EmailGenerator
 
 
 def main():
+    email = EmailGenerator()
     db_utils = UtilsDB()
     db_utils.create_new_models()
+
+    # eci_supermarket = ECISupermarketExtractor("https://www.elcorteingles.es/alimentacion/api/catalog/get-page/supermercado")
+    # eci_supermarket = eci_supermarket.iterate_thru_pages()
+    # db_utils.insert_dict_in_db(eci_supermarket, ECISupermarket)
+
+    # mercadona_supermarket = MercadonaExtractor("https://tienda.mercadona.es/api/categories/")
+    # mercadona_supermarket = mercadona_supermarket.iterate_thru_categories()
+    # db_utils.insert_dict_in_db(mercadona_supermarket, Mercadona)
+
     data_extractor = ECIGenericExtractor("https://www.elcorteingles.es/api/firefly/vuestore/products_list/")
     for category in data_extractor.categories:
-        category_dict = {}
         category_dict = data_extractor.iterate_thru_pages(category)
         category_model = db_utils.create_specific_model(category, category.replace("-", "_"), 'elCorteIngles', copy.deepcopy(default_cols))
         db_utils.insert_dict_in_db(category_dict, category_model)
-
-
-    eci_supermarket = ECISupermarketExtractor("https://www.elcorteingles.es/alimentacion/api/catalog/get-page/supermercado")
-    eci_supermarket = eci_supermarket.iterate_thru_pages()
-    db_utils.insert_dict_in_db(eci_supermarket, ECISupermarket)
-
-    mercadona_supermarket = MercadonaExtractor("https://tienda.mercadona.es/api/categories/")
-    mercadona_supermarket = mercadona_supermarket.iterate_thru_categories()
-    db_utils.insert_dict_in_db(mercadona_supermarket, Mercadona)
+        email_info = {'recipient_name': 'Ricardo', 'product_category': category, 'row_count': len(category_dict['id'])}
+        email.send_success_eci(email_info)
 
 # CONSUM
 # https://tienda.consum.es/api/rest/V1.0/shopping/category/menu
