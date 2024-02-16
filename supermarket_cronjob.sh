1d9c0fd4ea33e69f24e8b2a9f9df77f718cdd872
schrutebeet <ricardosans98@gmail.com>
2024-02-16 20:02:17 +0100

    Rename bash file

--- supermarket_cronjob.sh
+++ supermarket_cronjob.sh
@@ -7,5 +7,8 @@ export DATABASE_HOST=localhost
 # Change working directory
 cd /home/schrutebeet/Desktop/supermarket_data
 
+# Set up python environment
+source super_venv/bin/activate
+
 # Run main file
 python -m src.main
